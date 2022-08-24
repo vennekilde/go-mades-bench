d4f2d327bc9323424403ed69a26f851f6ba66f12
Jeppe Vennekilde <vennekilde@outlook.com>
2022-08-25 00:06:25 +0200

    Added tracing of all send events

--- main.go
+++ main.go
@@ -29,14 +29,16 @@ type Bencher struct {
 	goroutines   int
 	maxInTransit int
 
-	outboxReply       string
-	outbox            string
-	outboxSocketAddr  string
-	outboxAmqpUser    string
-	outboxAmqpPass    string
-	outboxClient      *amqp.Client
-	outboxSender      *amqp.Sender
-	outboxAckReceiver *amqp.Receiver
+	outboxSocketAddr    string
+	outboxAmqpUser      string
+	outboxAmqpPass      string
+	outboxReply         string
+	outbox              string
+	sendEvent           string
+	outboxClient        *amqp.Client
+	outboxSender        *amqp.Sender
+	outboxReplyReceiver *amqp.Receiver
+	sendEventReceiver   *amqp.Receiver
 
 	inbox           string
 	inboxSocketAddr string
@@ -45,28 +47,45 @@ type Bencher struct {
 	inboxClient     *amqp.Client
 	inboxReceiver   *amqp.Receiver
 
-	senderWg     sync.WaitGroup
-	receiverWg   sync.WaitGroup
-	timeStart    time.Time
-	timeEnd      time.Time
-	counter      int32
-	messageStats map[string]*MessageStats
+	senderWg        sync.WaitGroup
+	receiverWg      sync.WaitGroup
+	timeStart       time.Time
+	timeEnd         time.Time
+	messagesToSend  chan int32
+	counter         int32
+	receivedCounter int32
+	messageStats    map[string]*MessageStats
 }
 
 type MessageStats struct {
-	timeSent     time.Time
-	timeReceived time.Time
+	timeCreated        time.Time
+	timeAccepted       time.Time
+	timeReceived       time.Time
+	timeDeliveredEvent time.Time
+	timeReceivedEvent  time.Time
+}
+
+type FlightTimeStatistics struct {
+	DeliveryEvent FlightTimeStats
+	ReceivedEvent FlightTimeStats
+	Received      FlightTimeStats
+}
+
+type FlightTimeStats struct {
+	Average time.Duration
+	Median  time.Duration
 }
 
 func main() {
 	bencher := Bencher{}
 	flag.IntVar(&bencher.payloadSize, "size", 1000000, "Incompressible payload size to generate") //1000000 = 1mb,
 	flag.IntVar(&bencher.payloadCount, "n", 10000, "Number of messages to send")
-	flag.IntVar(&bencher.maxInTransit, "max-in-transit", -1, "Max messages allowed in transit. max-in-transit < 0 means unlimited")
+	flag.IntVar(&bencher.maxInTransit, "max-in-transit", 0, "Max messages allowed in transit. max-in-transit <= 0 means unlimited")
 	flag.IntVar(&bencher.goroutines, "goroutines", runtime.NumCPU(), "Number of go routines to use when sending")
 	flag.StringVar(&bencher.receiverCode, "receiver", "TEST-CODE", "Receiver Component Code")
 	flag.StringVar(&bencher.messageType, "message-type", "TEST-MESSAGE", "Message type to send messages with")
 
+	flag.StringVar(&bencher.sendEvent, "send-event", "ecp.endpoint.send.event", "send event queue")
 	flag.StringVar(&bencher.outboxReply, "outbox-reply", "ecp.endpoint.outbox.reply", "outbox reply queue")
 	flag.StringVar(&bencher.outbox, "outbox", "ecp.endpoint.outbox", "outbox queue")
 	flag.StringVar(&bencher.outboxSocketAddr, "outbox-addr", "amqp://localhost:5672", "Socket address to reach the internal broker")
@@ -87,8 +106,7 @@ func main() {
 	defer bencher.close()
 	bencher.prepareOutbox()
 	bencher.prepareInbox()
-	bencher.cleanInbox()
-	bencher.cleanOutbox()
+	bencher.cleanQueues()
 	bencher.startBenching()
 	bencher.printBenchResults()
 }
@@ -128,12 +146,21 @@ func (bencher *Bencher) prepareOutbox() {
 	}
 
 	// Create outbox reply receiver
-	bencher.outboxAckReceiver, err = recvSession.NewReceiver(
+	bencher.outboxReplyReceiver, err = recvSession.NewReceiver(
 		amqp.LinkSourceAddress(bencher.outboxReply),
 		amqp.LinkCredit(10),
 	)
 	if err != nil {
 		log.Fatal("Creating outbox receiver link:", err)
+	}
+
+	// Create send event receiver
+	bencher.sendEventReceiver, err = recvSession.NewReceiver(
+		amqp.LinkSourceAddress(bencher.sendEvent),
+		amqp.LinkCredit(10),
+	)
+	if err != nil {
+		log.Fatal("Creating send event receiver link:", err)
 	}
 }
 
@@ -168,32 +195,31 @@ func (bencher *Bencher) prepareInbox() {
 	}
 }
 
-func (bencher *Bencher) cleanInbox() {
-	// Drain inbox before testing
-	log.Println("Cleaning inbox before commencing test...")
-	for {
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-		msg, err := bencher.inboxReceiver.Receive(ctx)
-		cancel()
-		if err != nil {
-			break
-		}
-		bencher.inboxReceiver.AcceptMessage(context.Background(), msg)
-	}
+func (bencher *Bencher) cleanQueues() {
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	go bencher.cleanQueue(&wg, bencher.inboxReceiver)
+	go bencher.cleanQueue(&wg, bencher.outboxReplyReceiver)
+	go bencher.cleanQueue(&wg, bencher.sendEventReceiver)
+	wg.Wait()
 }
 
-func (bencher *Bencher) cleanOutbox() {
+func (bencher *Bencher) cleanQueue(wg *sync.WaitGroup, receiver *amqp.Receiver) {
 	// Drain  outbox reply before testing
-	log.Println("Cleaning outbox reply before commencing test...")
+	log.Printf("Cleaning %s before commencing test...\n", receiver.Address())
+	var i uint
 	for {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-		msg, err := bencher.outboxAckReceiver.Receive(ctx)
+		msg, err := receiver.Receive(ctx)
 		cancel()
 		if err != nil {
 			break
 		}
-		bencher.inboxReceiver.AcceptMessage(context.Background(), msg)
+		receiver.AcceptMessage(context.Background(), msg)
+		i++
 	}
+	log.Printf("Cleaned %d messages from queue %s\n", i, receiver.Address())
+	wg.Done()
 }
 
 func (bencher *Bencher) startBenching() {
@@ -205,15 +231,27 @@ func (bencher *Bencher) startBenching() {
 	log.Printf("Starting %s\n", bencher.timeStart.String())
 
 	bencher.sendMessages()
-	go bencher.receiveMessages()
-	go bencher.receiveAcks()
+	go bencher.receiveMessages(bencher.outboxReplyReceiver)
+	go bencher.receiveMessages(bencher.inboxReceiver)
+	go bencher.receiveMessages(bencher.sendEventReceiver)
 
 	bencher.senderWg.Wait()
 	bencher.receiverWg.Wait()
 	bencher.timeEnd = time.Now()
+
+	log.Println("waiting a second for any out of order messages")
+	time.Sleep(time.Second)
 }
 
 func (bencher *Bencher) sendMessages() {
+	initialMessagesToSend := bencher.payloadCount
+	if bencher.maxInTransit > 0 {
+		initialMessagesToSend = bencher.maxInTransit
+	}
+	bencher.messagesToSend = make(chan int32, initialMessagesToSend)
+	for i := 1; i <= initialMessagesToSend; i++ {
+		bencher.messagesToSend <- int32(i)
+	}
 	for k := 0; k < bencher.goroutines; k++ {
 		workerID := k
 		bencher.senderWg.Add(1)
@@ -222,6 +260,9 @@ func (bencher *Bencher) sendMessages() {
 			defer bencher.senderWg.Done()
 
 			for atomic.AddInt32(&bencher.counter, -1) >= 0 {
+				// Await for message to send
+				n := <-bencher.messagesToSend
+
 				payload := make([]byte, bencher.payloadSize)
 				fillPayloadWithData(payload)
 				ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
@@ -249,93 +290,156 @@ func (bencher *Bencher) sendMessages() {
 
 				bencher.m.Lock()
 				bencher.messageStats[correlationID] = &MessageStats{
-					timeSent: time.Now(),
+					timeCreated: time.Now(),
 				}
 				bencher.m.Unlock()
+				log.Printf("sent %d\n", n)
 			}
-			log.Printf("outbox worker #%d completed\n", workerID)
 		}()
 	}
 }
 
-func (bencher *Bencher) receiveMessages() {
-	for i := 0; i < bencher.payloadCount; i++ {
-		ctx := context.Background()
+func (bencher *Bencher) receiveMessages(receiver *amqp.Receiver) {
+	ctx := context.Background()
+	for {
 		// Receive next message
-		msg, err := bencher.inboxReceiver.Receive(ctx)
+		msg, err := receiver.Receive(ctx)
 		if err != nil {
 			log.Fatal("Reading message from AMQP:", err)
 		}
-		correlationID := msg.Properties.CorrelationID.(string)
+
+		bencher.handleMessage(msg, receiver.Address())
 
 		// Accept message
-		err = bencher.inboxReceiver.AcceptMessage(ctx, msg)
+		err = receiver.AcceptMessage(ctx, msg)
 		if err != nil {
 			log.Fatal("Accepting message from AMQP:", err)
 		}
-
-		bencher.m.Lock()
-		stats, ok := bencher.messageStats[correlationID]
-		if ok {
-			stats.timeReceived = time.Now()
-		}
-		bencher.m.Unlock()
-
-		bencher.receiverWg.Done()
-		log.Printf("received %d\n", i)
 	}
 }
 
-func (bencher *Bencher) calcFlightTime() (avg time.Duration, median time.Duration) {
+func (bencher *Bencher) handleMessage(msg *amqp.Message, queue string) {
+	var stats *MessageStats
+	var correlationID, messageID string
+	var ok bool
+
+	bencher.m.Lock()
+	if msg.Properties != nil && msg.Properties.CorrelationID != nil {
+		correlationID = msg.Properties.CorrelationID.(string)
+		stats = bencher.messageStats[correlationID]
+	}
+	if msg.ApplicationProperties != nil {
+		if messageID, ok = msg.ApplicationProperties["messageID"].(string); ok {
+			msgStats, ok := bencher.messageStats[messageID]
+			if !ok {
+				msgStats = &MessageStats{}
+				bencher.messageStats[messageID] = msgStats
+			}
+			if stats != nil {
+				msgStats.timeCreated = stats.timeCreated
+				msgStats.timeAccepted = stats.timeAccepted
+				delete(bencher.messageStats, correlationID)
+			}
+			stats = msgStats
+		}
+	}
+	bencher.m.Unlock()
+
+	if stats == nil {
+		log.Printf("unknown amqp message %#v", msg.ApplicationProperties)
+		return
+	}
+
+	switch queue {
+	case bencher.outboxReply:
+		stats.timeAccepted = time.Now()
+	case bencher.inbox:
+		stats.timeReceived = time.Now()
+	case bencher.sendEvent:
+		switch msg.Value.(string) {
+		case "DELIVERED":
+			stats.timeDeliveredEvent = time.Now()
+		case "RECEIVED":
+			if !stats.timeReceivedEvent.IsZero() {
+				log.Printf("message %s is already received\n", messageID)
+				break
+			}
+			stats.timeReceivedEvent = time.Now()
+			bencher.receiverWg.Done()
+			n := atomic.AddInt32(&bencher.receivedCounter, 1)
+			log.Printf("received %d\n", n)
+			bencher.messagesToSend <- int32(bencher.payloadCount) - bencher.counter
+		default:
+			log.Printf("unknown send event value: %s\n", msg.Value)
+		}
+	}
+}
+
+func (bencher *Bencher) calcFlightTime() FlightTimeStatistics {
+	return FlightTimeStatistics{
+		DeliveryEvent: bencher.calcFlightTimeStats("DELIVERED_EVENT"),
+		ReceivedEvent: bencher.calcFlightTimeStats("RECEIVED_EVENT"),
+		Received:      bencher.calcFlightTimeStats("RECEIVED"),
+	}
+}
+
+func (bencher *Bencher) calcFlightTimeStats(endStat string) FlightTimeStats {
+	var stats FlightTimeStats
 	var total time.Duration
 	flightTimes := make([]time.Duration, 0, bencher.payloadCount)
 	for key, val := range bencher.messageStats {
-		if val.timeReceived.IsZero() {
-			log.Printf("never received ack for msg: %s", key)
+		startTime := bencher.getTime(val, "CREATED")
+		if startTime.IsZero() {
+			log.Printf("never received %s for msg: %s", "CREATED", key)
 			continue
 		}
-		flightTime := val.timeReceived.Sub(val.timeSent)
+		endTime := bencher.getTime(val, endStat)
+		if endTime.IsZero() {
+			log.Printf("never received %s for msg: %s", endStat, key)
+			continue
+		}
+		flightTime := endTime.Sub(startTime)
 		flightTimes = append(flightTimes, flightTime)
 		total += flightTime
 	}
-	count := len(flightTimes)
-	// Avg
-	avg = total / time.Duration(count)
 
-	// Median
-	sort.Slice(flightTimes, func(i, j int) bool { return flightTimes[i] < flightTimes[j] })
-	mNumber := count / 2
-	if count%2 != 0 {
-		median = flightTimes[mNumber]
-	} else {
-		median = (flightTimes[mNumber-1] + flightTimes[mNumber]) / 2
+	count := len(flightTimes)
+	if count > 0 {
+		// Avg
+		stats.Average = total / time.Duration(count)
+
+		// Median
+		sort.Slice(flightTimes, func(i, j int) bool { return flightTimes[i] < flightTimes[j] })
+		mNumber := count / 2
+		if count%2 != 0 {
+			stats.Median = flightTimes[mNumber]
+		} else {
+			stats.Median = (flightTimes[mNumber-1] + flightTimes[mNumber]) / 2
+		}
 	}
 
-	return avg, median
+	return stats
 }
 
-func (bencher *Bencher) receiveAcks() {
-	/*for i := 0; i < bencher.payloadCount; i++ {
-		ctx := context.Background()
-		// Receive next message
-		msg, err := bencher.outboxAckReceiver.Receive(ctx)
-		if err != nil {
-			log.Fatal("Reading message from AMQP:", err)
-		}
-
-		// Accept message
-		err = bencher.inboxReceiver.AcceptMessage(ctx, msg)
-		if err != nil {
-			log.Fatal("Accepting message from AMQP:", err)
-		}
-		bencher.receiverWg.Done()
-		log.Printf("received %d\n", i)
-	}*/
+func (bencher *Bencher) getTime(stats *MessageStats, stat string) time.Time {
+	switch stat {
+	case "CREATED":
+		return stats.timeCreated
+	case "ACCEPTED":
+		return stats.timeAccepted
+	case "DELIVERED_EVENT":
+		return stats.timeDeliveredEvent
+	case "RECEIVED_EVENT":
+		return stats.timeReceivedEvent
+	case "RECEIVED":
+		return stats.timeReceived
+	}
+	return time.Time{}
 }
 
 func (bencher *Bencher) printBenchResults() {
 	duration := bencher.timeEnd.Sub(bencher.timeStart)
-	avgFlightTime, medianFlightTime := bencher.calcFlightTime()
+	flightTimes := bencher.calcFlightTime()
 	log.Printf("Started at       : %s\n", bencher.timeStart.String())
 	log.Printf("Ended at         : %s\n", bencher.timeEnd.String())
 	log.Printf("Duration         : %s\n", duration.String())
@@ -343,8 +447,9 @@ func (bencher *Bencher) printBenchResults() {
 	log.Printf("Msg size         : %d bytes\n", bencher.payloadSize)
 	log.Printf("Msg Throughput   : %f msgs/s\n", float64(bencher.payloadCount)/duration.Seconds())
 	log.Printf("Data Throughput  : %f mb/s (estimated)\n", float64(bencher.payloadCount)/duration.Seconds()*float64(bencher.payloadSize)/1000000)
-	log.Printf("Msg transit time : %s (avg)\n", avgFlightTime.String())
-	log.Printf("Msg transit time : %s (median)\n", medianFlightTime.String())
+	log.Printf("Msg transit time : %s (avg) %s (median) for Received\n", flightTimes.Received.Average.String(), flightTimes.Received.Median.String())
+	log.Printf("Msg transit time : %s (avg) %s (median) for Delivery Event\n", flightTimes.DeliveryEvent.Average.String(), flightTimes.DeliveryEvent.Median.String())
+	log.Printf("Msg transit time : %s (avg) %s (median) fpr Received Event\n", flightTimes.ReceivedEvent.Average.String(), flightTimes.ReceivedEvent.Median.String())
 }
 
 func (bencher *Bencher) close() {
